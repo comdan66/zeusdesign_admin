363c83e5f13c6f60cbf8e27aafeb9cf034680eed
OA Wu <comdan66@gmail.com>
2016-07-28 11:57:44 +0800

    Fix public js、css

--- application/views/public/public.js
+++ application/views/public/public.js
@@ -16,9 +16,27 @@ Array.prototype.min = function (k) {
   return Math.min.apply (null, this.column (k));
 };
 
+function getStorage (key) { return ((typeof (Storage) !== 'undefined') && (value = localStorage.getItem (key)) && (value = JSON.parse (value))) ? value : undefined; }
+function setStorage (key, data) { if (typeof (Storage) !== 'undefined') { localStorage.setItem (key, JSON.stringify (data)); return true; } return false; }
+
 window.ajaxError = function (result) {
   console.error (result.responseText);
 };
 
 $(function () {
+  $('._i').imgLiquid ({ verticalAlign:'center' });
+  window.vars = {};
+  window.funs = {};
+
+  window.funs.storage = {};
+  window.funs.storage.minMenu = {
+    storageKey: 'zeus.menu.min',
+    isMin: function (val) {
+      if (typeof val !== 'undefined') setStorage (this.storageKey, val);
+      var tmp = getStorage (this.storageKey);
+      return tmp ? tmp : false;
+    },
+  };
+
+  window.vars.$body = $('body');
 });
