1d7e5e1ba5638ae839d070bac084a1feb4a571a9
OA Wu <comdan66@gmail.com>
2016-07-28 11:58:35 +0800

    Add model uploaders.

---
+++ application/third_party/orm_uploaders/PromoCoverImageUploader.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2016 OA Wu Design
+ */
+
+class PromoCoverImageUploader extends OrmImageUploader {
+
+  public function getVersions () {
+    return array (
+        '' => array (),
+        '100x100c' => array ('adaptiveResizeQuadrant', 100, 100, 'c'),
+        '500w' => array ('resize', 500, 500, 'width')
+      );
+  }
+}
