c6b9e113c1ccd27aac53b611a614c79f81f6b9ce
OA Wu <comdan66@gmail.com>
2016-07-28 13:19:08 +0800

    Fix calendar

--- application/views/content/admin/calendar/index/content.php
+++ application/views/content/admin/calendar/index/content.php
@@ -1,77 +1,16 @@
+<input type='hidden' id='tags' data-val='<?php echo json_encode ($tags);?>' />
 <div class='panel'>
   <header>
-    <h2>新增分類</h2>
-    <a href='<?php echo base_url ($uri_1);?>' class='icon-x'></a>
+    <h2>行事曆</h2>
   </header>
 
-
-  <form class='form' method='post' action='<?php echo base_url ($uri_1);?>' enctype='multipart/form-data'>
-    <div class='row n2'>
-      <label>標題</label>
-      <div>
-        <input type='text' name='title' value='<?php echo isset ($posts['title']) ? $posts['title'] : '';?>' placeholder='請輸入標題..' maxlength='200' pattern='.{1,200}' required title='輸入名稱!' autofocus />
-      </div>
+  <div class='calendar'>
+    <div class='daySchedule'></div>
+    <div class='year_months'>
+      <a class='icon-al'></a>
+      <div class='title'></div>
+      <a class='icon-ar'></a>
     </div>
-    <div class='row n2'>
-      <label>內容</label>
-      <div>
-        <textarea name='content' class='pure autosize' placeholder='請輸入內容..'><?php echo isset ($posts['content']) ? $posts['content'] : '';?></textarea>
-      </div>
-    </div>
-    <div class='row n2'>
-      <label>鏈結</label>
-      <div>
-        <input type='text' name='link' value='<?php echo isset ($posts['link']) ? $posts['link'] : '';?>' placeholder='請輸入鏈結..' maxlength='200' pattern='.{1,200}' required title='輸入名稱!' autofocus />
-      </div>
-    </div>
-
-    <div class='row n2'>
-      <label>封面</label>
-      <div class='img'>
-
-        <div class='h'><img src='' /></div>
-
-        <div>
-          <input type='file' name='cover' />
-          <button type='button' class='file'>選擇檔案</button>
-        </div>
-
-      </div>
-    </div>
-    <div class='row n2'>
-      <label>開啟方式</label>
-      <div class='radios'>
-        <label>
-          <input type='radio' name='target' value='0' checked />
-          <span></span>
-          本頁
-        </label>
-        <label>
-          <input type='radio' name='target' value='1' />
-          <span></span>
-          分頁
-        </label>
-      </div>
-    </div>
-
-    <div class='row n2'>
-      <label>上、下架</label>
-      <div>
-        <label class='switch'>
-          <input type='checkbox' name='is_enabled' />
-          <span></span>
-        </label>
-      </div>
-    </div>
-
-    <div class='btns'>
-      <div class='row n2'>
-        <label></label>
-        <div>
-          <button type='reset'>取消</button>
-          <button type='submit'>送出</button>
-        </div>
-      </div>
-    </div>
-  </form>
+    <div class='months'></div>
+  </div>
 </div>
