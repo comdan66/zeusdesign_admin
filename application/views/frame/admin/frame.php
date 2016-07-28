66944382d02f149d782a66cfc80b203fd275a9a7
OA Wu <comdan66@gmail.com>
2016-07-28 11:57:10 +0800

    Fix admin's frame.

--- application/views/frame/admin/frame.php
+++ application/views/frame/admin/frame.php
@@ -13,6 +13,113 @@
   <body lang="zh-tw">
     <?php echo isset ($hidden_list) ? $hidden_list : ''; ?>
 
-    <?php echo isset ($content) ? $content : ''; ?>
+    <div id='container' class=''>
+      <div id='main_row'>
+        <div id='left_side'>
+          
+          <header>
+            <a href='<?php echo base_url ('index');?>'>Ｚ</a>
+            <span>Zeus Design Studio!</span>
+          </header>
+
+          <div id='login_user'>
+            <figure class='_i'>
+              <img src="<?php echo User::current ()->avatar ();?>">
+            </figure>
+            <div>
+              <span>Hi, 您好!</span>
+              <span><?php echo User::current ()->name;?></span>
+            </div>
+          </div>
+
+          <ul id='main_menu'>
+            <li>
+              <label>
+                <input type='checkbox' />
+                <span class='icon-ca'>行事曆</span>
+                <ul>
+                  <li><a href="<?php echo $url = base_url ('calendar');?>" class='icon-b<?php echo $now_url == $url ? ' active' : '';?>'>個人行程</a></li>
+                  <li><a href="<?php echo $url = base_url ('schedule-tags');?>" class='icon-ta<?php echo $now_url == $url ? ' active' : '';?>'>分類設定</a></li>
+                </ul>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input type='checkbox' />
+                <span class='icon-u'>人員管理</span>
+                <ul>
+                  <li><a href="" class='icon-ua'>權限設定</a></li>
+                </ul>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input type='checkbox' />
+                <span class='icon-f'>文章管理</span>
+                <ul>
+                  <li><a href="" class='icon-ta'>分類設定</a></li>
+                  <li><a href="" class='icon-fa'>文章設定</a></li>
+                </ul>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input type='checkbox' />
+                <span class='icon-ims'>首頁上搞</span>
+                <ul>
+                  <li><a href="<?php echo $url = base_url ('banners');?>" class='icon-im<?php echo $now_url == $url ? ' active' : '';?>'>Banner 設定</a></li>
+                  <li><a href="" class='icon-im'>Promo 設定</a></li>
+                </ul>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input type='checkbox' />
+                <span class='icon-g'>作品管理</span>
+                <ul>
+                  <li><a href="" class='icon-ta'>分類設定</a></li>
+                  <li><a href="" class='icon-g'>作品設定</a></li>
+                </ul>
+              </label>
+            </li>
+
+            <li>
+              <label>
+                <input type='checkbox' />
+                <span class='icon-ti'>帳務管理</span>
+                <ul>
+                  <li><a href="" class='icon-ta'>分類設定</a></li>
+                  <li><a href="" class='icon-ti'>帳務設定</a></li>
+                </ul>
+              </label>
+            </li>
+          </ul>
+
+        </div>
+        <div id='right_side'>
+          <div id='top_side'>
+            <button type='button' id='hamburger' class='icon-m'></button>
+            <span>
+              <a href='<?php echo base_url ('logout');?>' class='icon-o'></a>
+            </span>
+          </div>
+          <div id='main'>
+      <?php if ($_flash_danger = Session::getData ('_flash_danger', true)) { ?>
+              <div id='_flash_danger'><?php echo $_flash_danger;?></div>
+      <?php } else if ($_flash_info = Session::getData ('_flash_info', true)) { ?>
+              <div id='_flash_info'><?php echo $_flash_info;?></div>
+      <?php }?>
+      <?php echo isset ($content) ? $content : ''; ?>
+          </div>
+          <div id='bottom_side'>
+            後台版型設計 by 宙斯 <a href='http://www.ioa.tw/' target='_blank'>OA Wu</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
