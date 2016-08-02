9f2ff004dc9029cdadec8823a40c30e07a5aa295
OA Wu <comdan66@gmail.com>
2016-08-03 01:14:11 +0800

    Fix roures.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -11,11 +11,23 @@ Route::get ('/platform', 'platform@login');
 
 Route::group ('api', function () {
   Route::resource (array ('schedules'), 'schedules');
+  Route::resource (array ('schedule_tags'), 'schedule_tags');
 });
 
 Route::resourcePagination (array ('schedule_tags'), 'schedule_tags');
 Route::resourcePagination (array ('banners'), 'banners');
 Route::resourcePagination (array ('promos'), 'promos');
+Route::resourcePagination (array ('users'), 'users');
+
+Route::get ('/users/(:id)/show', 'users@show($1)');
+Route::get ('/users/(:id)/show/(:any)', 'users@show($1, $2)');
+Route::get ('/users/(:id)/show/(:any)/(:num)', 'users@show($1, $2, $3)');
+
+Route::get ('/calendar', 'index@calendar');
+Route::get ('/my', 'index@index');
+
+Route::get ('/my/(:any)', 'index@index($1)');
+Route::get ('/my/(:any)/(:num)', 'index@index($1, $2)');
 
 // echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
 // var_dump (Route::getRoute ());
