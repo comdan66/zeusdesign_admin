c6c5142703ee99b355e1c2d2ff4d8f79a728230d
OA Wu <comdan66@gmail.com>
2016-08-04 11:41:09 +0800

    Fix style.

--- application/views/frame/admin/frame.js
+++ application/views/frame/admin/frame.js
@@ -304,14 +304,12 @@ $(function () {
       };
       
       window.vars.$schedulePrompt.get (0).vars.$textarea.ckeditor ({
+        skin: 'oa',
         height: 100,
         resize_enabled: false,
         removePlugins: 'elementspath',
-        toolbarGroups: [
-            { name: '1', groups: [ 'mode', 'tools', 'links', 'basicstyles', 'colors', 'insert' ] },
-          ],
-        skin: 'oa',
-        removeButtons: 'Strike,Underline,Italic,Table,HorizontalRule,Smiley,Subscript,Superscript,Forms,Save,NewPage,Print,Preview,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Form,RemoveFormat,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,Flash,PageBreak,Iframe,About,Styles'
+        toolbarGroups: [{ name: '1', groups: [ 'mode', 'tools', 'links', 'basicstyles', 'colors', 'insert' ] }],
+        removeButtons: 'Image,Strike,Underline,Italic,Table,HorizontalRule,Smiley,Subscript,Superscript,Forms,Save,NewPage,Print,Preview,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Form,RemoveFormat,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,Flash,PageBreak,Iframe,About,Styles'
       });
 
       window.vars.$schedulePrompt.append (
@@ -394,13 +392,11 @@ $(function () {
   $('textarea.cke').ckeditor ({
     filebrowserUploadUrl: window.vars.apis.ckeditor.postImage (),
     filebrowserImageBrowseUrl: window.vars.apis.ckeditor.getImages (),
+    skin: 'oa',
     height: 300,
     resize_enabled: false,
     removePlugins: 'elementspath',
-    toolbarGroups: [
-        { name: '1', groups: [ 'mode', 'tools', 'links', 'basicstyles', 'colors', 'insert' ] },
-      ],
-    skin: 'oa',
+    toolbarGroups: [{ name: '1', groups: [ 'mode', 'tools', 'links', 'basicstyles', 'colors', 'insert' ] }],
     removeButtons: 'Strike,Underline,Italic,Table,HorizontalRule,Smiley,Subscript,Superscript,Forms,Save,NewPage,Print,Preview,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Form,RemoveFormat,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,Flash,PageBreak,Iframe,About,Styles'
   });
 
