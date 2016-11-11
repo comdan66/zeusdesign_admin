333d849bf3a55436d9004c930d25b876cce63ec5
OA Wu <comdan66@gmail.com>
2016-11-11 14:51:38 +0800

    Fix ckeditor.

--- application/controllers/admin/ckeditor.php
+++ application/controllers/admin/ckeditor.php
@@ -19,9 +19,7 @@ class Ckeditor extends Admin_controller {
     $funcNum = $_GET['CKEditorFuncNum'];
     $upload = OAInput::file ('upload');
 
-    if (!($upload && verifyCreateOrm ($img = CkeditorImage::create (array ('name' => ''))) && $img->name->put ($upload, true)))
-      echo "<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction ($funcNum, '', '上傳失敗！');</script>";
-    else
-      echo "<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction ($funcNum, '" . $img->name->url ('400h') . "', '上傳成功！');</script>";
+    if (!($upload && verifyCreateOrm ($img = CkeditorImage::create (array ('name' => ''))) && $img->name->put ($upload, true))) echo "<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction ($funcNum, '', '上傳失敗！');</script>";
+    else echo "<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction ($funcNum, '" . $img->name->url ('400h') . "', '上傳成功！');</script>";
   }
 }
