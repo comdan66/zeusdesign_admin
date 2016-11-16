cb9de43ca19c25abad0f6e3ccb94984fa3e303ba
OA Wu <comdan66@gmail.com>
2016-11-16 16:08:07 +0800

    Tmp.

--- application/views/content/admin/banners/index/content.php
+++ application/views/content/admin/banners/index/content.php
@@ -49,7 +49,7 @@
           <th width='150'>標題</th>
           <th>內容</th>
           <th width='150'>鏈結</th>
-          <th width='50' class='right'>排序</th>
+          <th width='50' class='center'>排序</th>
           <th width='85' class='right'>修改/刪除</th>
         </tr>
       </thead>
@@ -75,7 +75,7 @@
               <td><?php echo mini_link ($obj->link, 25);?></td>
 
 
-              <td class='right'>
+              <td class='center sort_btns'>
                 <a class='icon-tu' href='<?php echo base_url ($uri_1, $obj->id, 'sort', 'up');?>' data-method='post'></a>
                 <a class='icon-td' href='<?php echo base_url ($uri_1, $obj->id, 'sort', 'down');?>' data-method='post'></a>
               </td>
