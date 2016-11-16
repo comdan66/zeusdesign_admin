8a6fc81c6a52ed4ab21944933dc95c1d36eb6015
OA Wu <comdan66@gmail.com>
2016-11-17 00:10:00 +0800

    Tmp.

--- res/js/admin.js
+++ res/js/admin.js
@@ -454,7 +454,7 @@ $(function () {
     height: 300,
     resize_enabled: false,
     removePlugins: 'elementspath',
-    toolbarGroups: [{ name: '1', groups: [ 'mode', 'tools', 'links', 'basicstyles', 'colors', 'insert' ] }],
+    toolbarGroups: [{ name: '1', groups: [ 'mode', 'tools', 'links', 'basicstyles', 'colors', 'insert', 'list' ] }],
     removeButtons: 'Strike,Underline,Italic,Table,HorizontalRule,Smiley,Subscript,Superscript,Forms,Save,NewPage,Print,Preview,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Form,RemoveFormat,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,Flash,PageBreak,Iframe,About,Styles'
   });
 
