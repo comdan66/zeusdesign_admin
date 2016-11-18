f7abf3eecd9110780f4084564c0c7c30454c6555
OA Wu <comdan66@gmail.com>
2016-11-18 16:42:09 +0800

    Tmp.

--- application/controllers/admin/my_salaries.php
+++ application/controllers/admin/my_salaries.php
@@ -12,7 +12,7 @@ class My_salaries extends Admin_controller {
   public function __construct () {
     parent::__construct ();
 
-    if (!User::current ()->in_roles (array ('salary')))
+    if (!User::current ()->in_roles (array ('member')))
       return redirect_message (array ('admin'), array (
             '_flash_danger' => '您的權限不足，或者頁面不存在。'
           ));
