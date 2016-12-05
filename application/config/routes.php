47253f8e9efe8f6b51bcc747a7f45983c0983260
OA Wu <comdan66@gmail.com>
2016-12-05 18:12:20 +0800

    Tmp.

--- application/config/routes.php
+++ application/config/routes.php
@@ -71,8 +71,6 @@ Route::group ('api', function () {
   Route::resource (array ('schedules'), 'schedules');
   Route::resource (array ('schedule_tags'), 'schedule_tags');
   
-  Route::resource (array ('wallets'), 'wallets');
-  
   Route::resource (array ('spends'), 'spends');
   Route::resource (array ('spend_items'), 'spend_items');
 });
