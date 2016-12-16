555633974335ded759dc5f7b13da7141d774d41b
OA Wu <comdan66@gmail.com>
2016-12-16 11:52:18 +0800

    Finish Notification feature.

--- application/migrations/034_add_task_commits.php
+++ application/migrations/034_add_task_commits.php
@@ -13,6 +13,7 @@ class Migration_Add_task_commits extends CI_Migration {
         `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
         `task_id` int(11) unsigned NOT NULL DEFAULT 0 COMMENT 'Task ID',
         `user_id` int(11) unsigned NOT NULL DEFAULT 0 COMMENT 'Commit 的人',
+        `action` varchar(150) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '動作',
         `content` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '內容',
         `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '更新時間',
         `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '新增時間',
