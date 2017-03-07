7c8aa4e0797b3a62f5b9a4875de81acf63408c80
OA Wu <comdan66@gmail.com>
2017-03-07 16:49:03 +0800

    Add line SDK

--- application/controllers/api/lines.php
+++ application/controllers/api/lines.php
@@ -8,6 +8,8 @@
 require FCPATH . 'vendor/autoload.php';
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
 
 class Lines extends Api_controller {
 
@@ -32,61 +34,26 @@ class Lines extends Api_controller {
     $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
     $body = file_get_contents ("php://input");
 
-    $events = $bot->parseEventRequest ($body, $signature);
     
-    write_file ($path, '===============');
-
-echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
-var_dump ($signature, $body);
-exit ();
-
-
-
-
-
-
-
-
-
-    // $text = $receive->result[0]->content->text;
-    // $from = $receive->result[0]->content->from;
-    // $content_type = $receive->result[0]->content->contentType;
-    write_file ($path, $headers['X-line-signature']);
-    // write_file ($path, json_encode ($receive));
-    // write_file ($path, json_encode ($this->input->request_headers()));
-    // write_file ($path, json_encode (OAInput::get ()));
-    // echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
-    // var_dump (LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE);
-    // {"X-line-signature":"joFU91e1koizDtlJs5gRjENNvPuk06\/SJmDZ131G+k4=","Content-type":"application\/json;charset=UTF-8","Content-length":"233","Host":"admin.zeusdesign.com.tw","Accept":"*\/*","User-agent":"LineBotWebhook\/1.0"}
-    exit ();
-
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder ('hello');
-    $response = $bot->replyMessage('<reply token>', $textMessageBuilder);
-    if ($response->isSucceeded ()) {
-        echo 'Succeeded!';
-        return;
+    try {
+      $events = $bot->parseEventRequest($req->getBody(), $signature[0]);
+    } catch (Exception $e) {
+      write_file ($path, '===> Error, Events Error! Msg:' . $e->getMessage ());
+      exit ();
     }
 
-// Failed
-echo $response->getHTTPStatus . ' ' . $response->getRawBody();
-
-
-    // /* 準備Post回Line伺服器的資料 */
-    // $header = ["Content-Type: application/json; charser=UTF-8", "X-Line-ChannelID:" . $channel_id, "X-Line-ChannelSecret:" . $channel_secret, "X-Line-Trusted-User-With-ACL:" . $mid];
-    // $message = $this->getBoubouMessage ($text);
-    // $this->sendMessage ($header, $from, $message);
-    // echo "OK";
-  }
-    /* 發送訊息 */
-  private function sendMessage($header, $to, $message) {
-    $url = "https://trialbot-api.line.me/v1/events";
-    $data = ["to" => [$to], "toChannel" => 1383378250, "eventType" => "138311608800106203", "content" => ["contentType" => 1, "toType" => 1, "text" => $message]];
-    $context = stream_context_create(array(
-    "http" => array("method" => "POST", "header" => implode(PHP_EOL, $header), "content" => json_encode($data), "ignore_errors" => true)
-    ));
-    file_get_contents($url, false, $context);
-  }
-  private function getBoubouMessage($value){    
-    return "寶寶" . $value ."，只是寶寶不說";
+    foreach ($events as $event) {
+      if (!($event instanceof MessageEvent)) {
+        write_file ($path, '===> Log:' . 'Non message event has come');
+        continue;
+      }
+      if (!($event instanceof TextMessage)) {
+        write_file ($path, '===> Log:' . 'Non text message has come');
+        continue;
+      }
+      $replyText = $event->getText ();
+      $resp = $bot->replyText ($event->getReplyToken (), $replyText);
+    }
+    echo "OK";
   }
 }
