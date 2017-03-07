89e1fa1734073344337f7757840c3b684122fcc4
OA Wu <comdan66@gmail.com>
2017-03-07 15:51:12 +0800

    Add line SDK

---
+++ application/controllers/api/lines.php
@@ -0,0 +1,19 @@
+<?php defined ('BASEPATH') OR exit ('No direct script access allowed');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2016 OA Wu Design
+ * @link        http://www.ioa.tw/
+ */
+require FCPATH . 'vendor/autoload.php';
+
+class Lines extends Api_controller {
+
+  public function __construct () {
+    parent::__construct ();
+    
+  }
+  public function index () {
+    
+  }
+}
