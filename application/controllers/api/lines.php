0ce7b542dc65ed4aca98e3db9985c07ab4ad687a
OA Wu <comdan66@gmail.com>
2017-03-07 16:01:01 +0800

    Add line SDK

--- application/controllers/api/lines.php
+++ application/controllers/api/lines.php
@@ -14,6 +14,35 @@ class Lines extends Api_controller {
     
   }
   public function index () {
+    $channel_id = "Channel ID";
+    $channel_secret = "Channel Secret";
+    $mid = "MID";
+     
+    /* 將收到的資料整理至變數 */
+    $receive = json_decode (file_get_contents ("php://input"));
+    $text = $receive->result[0]->content->text;
+    $from = $receive->result[0]->content->from;
+    $content_type = $receive->result[0]->content->contentType;
     
+    echo '<meta http-equiv="Content-type" content="text/html; charset=utf-8" /><pre>';
+    var_dump ($text, $from, $content_type);
+    exit ();
+    // /* 準備Post回Line伺服器的資料 */
+    // $header = ["Content-Type: application/json; charser=UTF-8", "X-Line-ChannelID:" . $channel_id, "X-Line-ChannelSecret:" . $channel_secret, "X-Line-Trusted-User-With-ACL:" . $mid];
+    // $message = $this->getBoubouMessage ($text);
+    // $this->sendMessage ($header, $from, $message);
+    // echo "OK";
+  }
+    /* 發送訊息 */
+  private function sendMessage($header, $to, $message) {
+    $url = "https://trialbot-api.line.me/v1/events";
+    $data = ["to" => [$to], "toChannel" => 1383378250, "eventType" => "138311608800106203", "content" => ["contentType" => 1, "toType" => 1, "text" => $message]];
+    $context = stream_context_create(array(
+    "http" => array("method" => "POST", "header" => implode(PHP_EOL, $header), "content" => json_encode($data), "ignore_errors" => true)
+    ));
+    file_get_contents($url, false, $context);
+  }
+  private function getBoubouMessage($value){    
+    return "寶寶" . $value ."，只是寶寶不說";
   }
 }
