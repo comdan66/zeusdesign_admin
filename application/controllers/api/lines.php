9551dbec52261f66524ee89f226bf6a90c767d7e
OA Wu <comdan66@gmail.com>
2017-03-07 17:47:24 +0800

    Add line SDK

--- application/controllers/api/lines.php
+++ application/controllers/api/lines.php
@@ -74,10 +74,16 @@ class Lines extends Api_controller {
 
 
       // $messageBuilder = new LocationMessageBuilder ('my location', '〒150-0002 東京都渋谷区渋谷２丁目２１−１', 35.65910807942215, 139.70372892916203);
-      $messageBuilder = new ButtonTemplateBuilder ('你好', '測試一下', 'http://pic.mazu.ioa.tw/upload/pictures/name/0/0/4/32/2048w_1470185053_5700966ba81a8.jpg', array (
-          new UriTemplateActionBuilder ("View detail", 'www.ioa.tw'),
-        ));
+      try {
+        $messageBuilder = new ButtonTemplateBuilder ('你好', '測試一下', 'http://pic.mazu.ioa.tw/upload/pictures/name/0/0/4/32/2048w_1470185053_5700966ba81a8.jpg', array (
+            new UriTemplateActionBuilder ("View detail", 'www.ioa.tw'),
+          ));
+      } catch (Exception $e) {
+        write_file ($path, '===> Error, Builder Error! Msg:' . $e->getMessage () . "\n", FOPEN_READ_WRITE_CREATE);
+        exit ();
+      }
       write_file ($path, 'Data OK3..' . "\n", FOPEN_READ_WRITE_CREATE);
+
       $resp = $bot->replyMessage ($event->getReplyToken (), $messageBuilder);
       
       if ($response->isSucceeded ()) {
