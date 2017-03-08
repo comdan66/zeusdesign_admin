7c3509c00099baea2d3e8769cb735c8cc8cba023
OA Wu <comdan66@gmail.com>
2017-03-08 13:10:35 +0800

    Add line SDK

--- application/models/LinebotMsg.php
+++ application/models/LinebotMsg.php
@@ -6,9 +6,9 @@
  * @link        http://www.ioa.tw/
  */
 
-class LinebotLog extends OaModel {
+class LinebotMsg extends OaModel {
 
-  static $table_name = 'linebot_logs';
+  static $table_name = 'linebot_msgs';
 
   static $has_one = array (
   );
@@ -17,14 +17,6 @@ class LinebotLog extends OaModel {
   );
 
   static $belongs_to = array (
-  );
-
-  const NO_ECHO = 0;
-  const IS_ECHO = 1;
-
-  static $okNames = array (
-    self::NO_ECHO => '未回傳',
-    self::IS_ECHO => '已經回傳',
   );
 
   public function __construct ($attributes = array (), $guard_attributes = true, $instantiating_via_find = false, $new_record = true) {
