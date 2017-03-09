75e4c44f5f678b89ead3f349c90b7d2286abb5e3
OA Wu <comdan66@gmail.com>
2017-03-09 17:50:15 +0800

    Fix code

--- application/migrations/053_add_linebot_log_audios.php
+++ application/migrations/053_add_linebot_log_audios.php
@@ -12,11 +12,6 @@ class Migration_Add_linebot_log_audios extends CI_Migration {
         `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
         `linebot_log_id` int(11) unsigned NOT NULL DEFAULT 0 COMMENT 'Line Bot Log ID',
         
-        `title` varchar(50) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '標題',
-        `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '地址',
-        `latitude` DOUBLE NOT NULL COMMENT '緯度',
-        `longitude` DOUBLE NOT NULL COMMENT '經度',
-
         `updated_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '更新時間',
         `created_at` datetime NOT NULL DEFAULT '" . date ('Y-m-d H:i:s') . "' COMMENT '新增時間',
         PRIMARY KEY (`id`)
